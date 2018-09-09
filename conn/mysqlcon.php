987d7a9e532d1f8a0da2e3912e685e2235bf5d94
truank <truank1331dunk1331@gmail.com>
2018-09-09 17:51:30 +0700

    first commit

--- conn/mysqlcon.php
+++ conn/mysqlcon.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "MyDBest";
+$servername = "plearnjai.com";
+$username = "plearnja_cpsu4";
+$password = "0oyQt3aNOD";
+$dbname = "plearnja_cpsu4";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
